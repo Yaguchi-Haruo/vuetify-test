854cdb3250de6c28c19f9b12c83ef7a655bfb962
Yaguchi Haruo <takasumi.nakamura.2177@gmail.com>
2021-03-14 23:43:56 +0900

    ・テーマを切り替えるスイッチを設置 ・vuetifyでスイッチコントローラの色を並べて再現

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-switch
+        v-model="theme"
+        :prepend-icon="themeIcon"
+        style="float: right"
+      ></v-switch>
+      <HelloWorld />
+    </v-main>
+  </v-app>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue, Watch } from "vue-property-decorator";
 import HelloWorld from "./components/HelloWorld.vue";
 
 @Component({
@@ -14,16 +53,17 @@ import HelloWorld from "./components/HelloWorld.vue";
     HelloWorld
   }
 })
-export default class App extends Vue {}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+export default class App extends Vue {
+  // テーマ切り替え変数
+  private theme = false;
+  // テーマ切り替えボタンが押されたときに実行
+  get themeIcon(): string {
+    return this.theme ? "mdi-weather-night" : "mdi-weather-sunny";
+  }
+  // テーマ切り替え変数の変更を検知して実行
+  @Watch("theme")
+  onPushedThemeButton() {
+    this.$vuetify.theme.dark = this.theme;
+  }
 }
-</style>
+</script>
---
+++ src/components/CardMenu.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card elevation="2" :color="getColor">
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="overline mb-4">
+          OVERLINE
+        </div>
+        <v-list-item-title class="headline mb-1">
+          Headline 5
+        </v-list-item-title>
+        <v-list-item-subtitle
+          >Greyhound divisely hello coldly fonwderfully
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class CardMenu extends Vue {
+  @Prop()
+  public color: string | undefined;
+  get getColor() {
+    return this.color;
+  }
+}
+</script>
